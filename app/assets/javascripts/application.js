8995982f0e7bda8aeb9333e3ea249bdd4ecbd943
Learnor <learnor@gmail.com>
2017-02-27 18:45:26 -0600

    user can login/logout/signup"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
+//= requrie bootstrap/alert
+//= require bootstrap/dropdown
 //= require turbolinks
 //= require_tree .
